fb7d2ee4e51bb254560703b99d23ad9f85035ce7
Aric Mazick <aricmazick@Arics-iMac.fullchannel.net>
2024-05-14 20:53:02 -0400

    .

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -32,7 +32,7 @@ export default function Home() {
           <div className="h-1 mx-auto gradient w-64 opacity-25 my-0 py-0 rounded-t"></div>
         </div>
         <div className="flex flex-wrap">
-          <div className="w-5/6 sm:w-1/2 p-6">
+          <div className="w-full sm:w-1/2 p-6">
             <h3 className="text-3xl text-gray-800 font-bold leading-none mb-3">
               Engaging experiences for modern learners
             </h3>
@@ -170,7 +170,7 @@ export default function Home() {
         </div>
         <div className="flex flex-wrap flex-col-reverse sm:flex-row">
           <div className="w-full sm:w-1/2 p-6 mt-6">
-            <svg className="w-5/6 sm:h-64 mx-auto" viewBox="0 0 1176.60617 873.97852" xmlns="http://www.w3.org/2000/svg">
+            <svg className="w-full sm:h-64 mx-auto" viewBox="0 0 1176.60617 873.97852" xmlns="http://www.w3.org/2000/svg">
               <title>connected world</title>
               <path
                 transform="translate(-11.697 -13.011)"
