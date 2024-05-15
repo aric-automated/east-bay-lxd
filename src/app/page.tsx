6dd968c2427d110eec94eec6a077011b316cdb27
Aric Mazick <aricmazick@Arics-iMac.fullchannel.net>
2024-05-14 20:58:07 -0400

    .

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -26,7 +26,7 @@ export default function Home() {
       <section className="bg-white border-b py-8">
       <div className="container max-w-5xl mx-auto m-8">
         <h2 className="w-full my-2 text-5xl font-bold leading-tight text-center text-gray-800">
-          Title
+          Why East Bay Learning Design?
         </h2>
         <div className="w-full mb-4">
           <div className="h-1 mx-auto gradient w-64 opacity-25 my-0 py-0 rounded-t"></div>
