83ac6bbf9c13ffbb5814c6c2ef2c102afbad64a2
Aric Mazick <aricmazick@Arics-iMac.fullchannel.net>
2024-05-18 14:45:23 -0400

    .

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -38,7 +38,7 @@ export default function Home() {
               src="/1.png"
             />
           </div>
-          <div className="w-full sm:w-1/2 p-6">
+          <div className="w-full sm:w-2/3 p-6">
             <h3 className="text-3xl text-gray-800 font-bold leading-none mb-3">
               Engaging experiences for modern learners
             </h3>
