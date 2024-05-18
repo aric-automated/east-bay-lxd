c5f2dcde5f0c4a6590f1119a90a317e0e3d1349d
Aric Mazick <aricmazick@Arics-iMac.fullchannel.net>
2024-05-18 15:22:29 -0400

    .

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -23,90 +23,91 @@ export default function Home() {
           src="/maple-tutorial.png"
         />
       </div>
-      <section className="bg-white border-b py-8">
-      <div className="container max-w-5xl mx-auto m-8">
-        <h2 className="w-full my-2 text-5xl font-bold leading-tight text-center text-gray-800">
-          Why East Bay Learning Design?
-        </h2>
-        <div className="w-full mb-4"></div>
-        <div className="flex flex-wrap justify-center">
-          <div className="pr-6 py-12 flex justify-end">
+    
+      <section className="bg-white border-t py-8">
+        <div className="container max-w-5xl mx-auto m-8">
+          <h2 className="w-full my-2 text-5xl font-bold leading-tight text-center text-gray-800">
+            Why East Bay Learning Design?
+          </h2>
+          <div className="w-full mb-4"></div>
+          <div className="flex flex-wrap justify-center">
+            <div className="pr-6 py-12 flex justify-end">
+                <img className="w-[79px] h-[92px] object-contain"
+                  width={79}
+                  height={92}
+                  alt="Image of a stylized 1"
+                  src="/1.png"
+                />
+            </div>
+            <div className="w-full sm:w-2/3 p-6">
+              <h3 className="text-3xl text-gray-800 font-bold leading-none mb-3">
+                Engaging experiences for modern learners
+              </h3>
+              <p className="text-justify text-gray-600 mb-8">
+                At East Bay Learning Design, we specialize in creating e-learning experiences that captivate and inspire. Our expert team combines innovative design thinking with cutting-edge technology to deliver impactful learning solutions. Whether you&apos;re a small business aiming to scale up, or a large corporate organization in need of support, we&apos;re here to help you succeed.
+              </p>
+            </div>
+          </div>
+
+          <div className="flex flex-wrap justify-center">
+            <div className="w-full sm:w-2/3 p-6">
+              <h3 className="text-3xl text-gray-800 font-bold leading-none mb-3">
+                Simplified learning with mobile first deployment
+              </h3>
+              <p className="text-justify text-gray-600 mb-8">
+                We empower learners to access content anytime, anywhere. Our simple, mobile-first methodology ensures seamless content deployment and an optimized learning experience. With our e-learning technology, you can deliver training that aligns with the on-the-go workday of today&apos;s learners, maximizing engagement and knowledge retention.
+              </p>
+            </div>
+            <div className="px-6 py-12 flex justify-end">
               <img className="w-[79px] h-[92px] object-contain"
                 width={79}
                 height={92}
-                alt="Image of a stylized 1"
-                src="/1.png"
+                alt="Image of a stylized 2"
+                src="/2.png"
               />
+            </div>
           </div>
-          <div className="w-full sm:w-2/3 p-6">
-            <h3 className="text-3xl text-gray-800 font-bold leading-none mb-3">
-              Engaging experiences for modern learners
-            </h3>
-            <p className="text-justify text-gray-600 mb-8">
-              At East Bay Learning Design, we specialize in creating e-learning experiences that captivate and inspire. Our expert team combines innovative design thinking with cutting-edge technology to deliver impactful learning solutions. Whether you&apos;re a small business aiming to scale up, or a large corporate organization in need of support, we&apos;re here to help you succeed.
-            </p>
-          </div>
-        </div>
 
-        <div className="flex flex-wrap justify-center">
-          <div className="w-full sm:w-2/3 p-6">
-            <h3 className="text-3xl text-gray-800 font-bold leading-none mb-3">
-              Simplified learning with mobile first deployment
-            </h3>
-            <p className="text-justify text-gray-600 mb-8">
-              We empower learners to access content anytime, anywhere. Our simple, mobile-first methodology ensures seamless content deployment and an optimized learning experience. With our e-learning technology, you can deliver training that aligns with the on-the-go workday of today&apos;s learners, maximizing engagement and knowledge retention.
-            </p>
+          <div className="flex flex-wrap justify-center">
+            <div className="px-6 py-12 flex justify-end">
+              <img className="w-[79px] h-[92px] object-contain"
+                width={79}
+                height={92}
+                alt="Image of a stylized 3"
+                src="/3.png"
+              />
+            </div>
+            <div className="w-full sm:w-2/3 p-6">
+              <h3 className="text-3xl text-gray-800 font-bold leading-none mb-3">
+                Iterative development, lasting impact
+              </h3>
+              <p className="text-justify text-gray-600 mb-8">
+              Experience our iterative development methodology that drives continuous improvement and lasting impact. We believe in delivering rapid results without compromising quality. Drawing from our expertise across industries and our presence as speakers at renowned learning conferences, we bring a wealth of knowledge to each project.
+              </p>
+            </div>
           </div>
-          <div className="px-6 py-12 flex justify-end">
-            <img className="w-[79px] h-[92px] object-contain"
-              width={79}
-              height={92}
-              alt="Image of a stylized 2"
-              src="/2.png"
-            />
-          </div>
-        </div>
 
-        <div className="flex flex-wrap justify-center">
-          <div className="px-6 py-12 flex justify-end">
-            <img className="w-[79px] h-[92px] object-contain"
-              width={79}
-              height={92}
-              alt="Image of a stylized 3"
-              src="/3.png"
-            />
+          <div className="flex flex-wrap justify-center">
+            <div className="w-full sm:w-2/3 p-6">
+              <h3 className="text-3xl text-gray-800 font-bold leading-none mb-3">
+                Tailored solutions for your objectives
+              </h3>
+              <p className="text-justify text-gray-600 mb-8">
+                Accelerate learning outcomes with our customized solutions tailored to your specific needs. We create personalized and immersive learning experiences that resonate with your learners. Every client is unique, and our commitment is to help you rapidly deploy content that enables you to achieve your operational and developmental objectives.
+              </p>
+            </div>
+            <div className="px-6 py-12 flex justify-end">
+              <img className="w-[79px] h-[92px] object-contain"
+                width={79}
+                height={92}
+                alt="Image of a stylized 4"
+                src="/4.png"
+              />
+            </div>
           </div>
-          <div className="w-full sm:w-2/3 p-6">
-            <h3 className="text-3xl text-gray-800 font-bold leading-none mb-3">
-              Iterative development, lasting impact
-            </h3>
-            <p className="text-justify text-gray-600 mb-8">
-            Experience our iterative development methodology that drives continuous improvement and lasting impact. We believe in delivering rapid results without compromising quality. Drawing from our expertise across industries and our presence as speakers at renowned learning conferences, we bring a wealth of knowledge to each project.
-            </p>
-          </div>
-        </div>
 
-        <div className="flex flex-wrap justify-center">
-          <div className="w-full sm:w-2/3 p-6">
-            <h3 className="text-3xl text-gray-800 font-bold leading-none mb-3">
-              Tailored solutions for your objectives
-            </h3>
-            <p className="text-justify text-gray-600 mb-8">
-              Accelerate learning outcomes with our customized solutions tailored to your specific needs. We create personalized and immersive learning experiences that resonate with your learners. Every client is unique, and our commitment is to help you rapidly deploy content that enables you to achieve your operational and developmental objectives.
-            </p>
-          </div>
-          <div className="px-6 py-12 flex justify-end">
-            <img className="w-[79px] h-[92px] object-contain"
-              width={79}
-              height={92}
-              alt="Image of a stylized 4"
-              src="/4.png"
-            />
-          </div>
         </div>
-
-      </div>
-    </section>
+      </section>
     </div>
   )
 }
