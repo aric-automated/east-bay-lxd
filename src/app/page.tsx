b0e3a0310548866c341f0d2fa2a9ff9cdd1e3c9c
Aric Mazick <aricmazick@Arics-iMac.fullchannel.net>
2024-05-18 14:53:39 -0400

    .

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -29,8 +29,8 @@ export default function Home() {
           Why East Bay Learning Design?
         </h2>
         <div className="w-full mb-4"></div>
-        <div className="flex flex-wrap justify-center bg-green-100">
-          <div className="px-6 py-12 flex justify-end bg-slate-100">
+        <div className="flex flex-wrap justify-center">
+          <div className="px-6 py-12 flex justify-end">
             <img className="w-[79px] h-[92px] object-contain"
               width={79}
               height={92}
@@ -48,8 +48,8 @@ export default function Home() {
           </div>
         </div>
 
-        <div className="flex flex-wrap justify-center bg-green-100">
-          <div className="px-6 py-12 flex justify-end bg-slate-100">
+        <div className="flex flex-wrap justify-center">
+          <div className="px-6 py-12 flex justify-end">
             <img className="w-[79px] h-[92px] object-contain"
               width={79}
               height={92}
@@ -63,6 +63,44 @@ export default function Home() {
             </h3>
             <p className="text-gray-600 mb-8">
               We empower learners to access content anytime, anywhere. Our simple, mobile-first methodology ensures seamless content deployment and an optimized learning experience. With our e-learning technology, you can deliver training that aligns with the on-the-go workday of today&apos;s learners, maximizing engagement and knowledge retention.
+            </p>
+          </div>
+        </div>
+
+        <div className="flex flex-wrap justify-center">
+          <div className="px-6 py-12 flex justify-end">
+            <img className="w-[79px] h-[92px] object-contain"
+              width={79}
+              height={92}
+              alt="Image of a stylized 3"
+              src="/3.png"
+            />
+          </div>
+          <div className="w-full sm:w-2/3 p-6">
+            <h3 className="text-3xl text-gray-800 font-bold leading-none mb-3">
+              Iterative development, lasting impact
+            </h3>
+            <p className="text-gray-600 mb-8">
+            Experience our iterative development methodology that drives continuous improvement and lasting impact. We believe in delivering rapid results without compromising quality. Drawing from our expertise across industries and our presence as speakers at renowned learning conferences, we bring a wealth of knowledge to each project.
+            </p>
+          </div>
+        </div>
+
+        <div className="flex flex-wrap justify-center">
+          <div className="px-6 py-12 flex justify-end">
+            <img className="w-[79px] h-[92px] object-contain"
+              width={79}
+              height={92}
+              alt="Image of a stylized 4"
+              src="/4.png"
+            />
+          </div>
+          <div className="w-full sm:w-2/3 p-6">
+            <h3 className="text-3xl text-gray-800 font-bold leading-none mb-3">
+              Tailored solutions for your objectives
+            </h3>
+            <p className="text-gray-600 mb-8">
+              Accelerate learning outcomes with our customized solutions tailored to your specific needs. We create personalized and immersive learning experiences that resonate with your learners. Every client is unique, and our commitment is to help you rapidly deploy content that enables you to achieve your operational and developmental objectives.
             </p>
           </div>
         </div>
