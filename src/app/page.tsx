052d243a04a3e0f7bcf2700efbfa0b39a8476d73
Aric Mazick <aricmazick@Arics-iMac.fullchannel.net>
2024-05-18 14:37:22 -0400

    .

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -40,7 +40,7 @@ export default function Home() {
               src="/1.png"
             />
           </div>
-          <div className="w-full sm:w-1/2 p-6">
+          <div className="w-full p-6">
             <h3 className="text-3xl text-gray-800 font-bold leading-none mb-3">
               Engaging experiences for modern learners
             </h3>
@@ -49,8 +49,8 @@ export default function Home() {
             </p>
           </div>
         </div>
-        <div className="flex flex-wrap flex-col-reverse sm:flex-row">
-          <div className="w-full sm:w-1/2 p-6 mt-6">
+        <div className="flex flex-wrap justify-center bg-green-100">
+          <div className="px-6 py-12 mt-6 flex justify-end bg-slate-100">
             <img className="object-right"
                 width={79}
                 height={92}
@@ -58,15 +58,13 @@ export default function Home() {
                 src="/2.png"
               />
           </div>
-          <div className="w-full sm:w-1/2 p-6 mt-6">
-            <div className="align-middle">
+          <div className="w-full p-6 mt-6">
               <h3 className="text-3xl text-gray-800 font-bold leading-none mb-3">
                 Simplified learning with mobile first deployment
               </h3>
               <p className="text-gray-600 mb-8">
                 We empower learners to access content anytime, anywhere. Our simple, mobile-first methodology ensures seamless content deployment and an optimized learning experience. With our e-learning technology, you can deliver training that aligns with the on-the-go workday of today&apos;s learners, maximizing engagement and knowledge retention.
               </p>
-            </div>
           </div>
         </div>
       </div>
